92d1d5a8fcef7daeca7dfe07b2f0aa8542e4cfa9
Andre Dos Santos <afdossa@clemson.edu>
2025-11-04 15:21:57 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -21,7 +21,7 @@ const App: React.FC = () => {
   });
 
   useEffect(() => {
-    fetch('real_estate_data.json')
+      fetch(`${import.meta.env.BASE_URL}real_estate_data.json`)
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
